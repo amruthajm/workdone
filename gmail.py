350cdd9266bddbc572c78366b421e4f64a778c76
amruthajm <43128144+amruthajm@users.noreply.github.com>
2018-10-28 16:08:05 +0530

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from httplib2 import Http
+ 
+ 
+fromaddr = "xyz@gmail.com"
+toaddr = "pqr56@gmail.com"
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "Status Update 28-10-2018"
+ 
+body = "This email is sent via python code"
+msg.attach(MIMEText(body, 'plain'))
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr,"******")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
