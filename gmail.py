a7ee38e004223f7d9ccecdb3bf18444ab9a090ec
amruthajm <43128144+amruthajm@users.noreply.github.com>
2018-10-28 21:38:10 +0530

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -16,7 +16,7 @@ msg.attach(MIMEText(body, 'plain'))
  
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(fromaddr,"******")
+server.login(fromaddr,"your password")
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
 server.quit()
